0d79406f4b677b8f7cf15dc0891a87892289c08d
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-11-20 11:41:10 -0700

    working on routes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+  
 from flask import Flask, jsonify
 import numpy as np
 
@@ -9,7 +10,28 @@ from sqlalchemy import create_engine, func
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 
-# reflect an existing database into a new model
+# reflect the existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
+
+# Save references for each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Flask initiation
+app = Flask(__name__)
+
+# Flask routes
+
+@app.route("/")
+def welcome():
+    return(
+        f"Welcome to your historical Hawaii Weather homepage"
+        f"You can browse:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[end_date format:yyyy-mm-dd]<br/>"
+    )
